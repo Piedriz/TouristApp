f39fd6b4baadb6c688e90c3bf22af77ad244f4a9
Santiago Piedriz <piedrizpasos@gmail.com>
2022-11-17 23:15:07 -0500

    preparando deploy

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { useState } from "react";
 
 export default function Searchbar({ searches, setSearches, size }) {
   function handleSearch(e) {
--- src/database.js
+++ src/database.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
-const URI = 'mongodb+srv://Piedriz:Arepa300-@touristapp-db.gcjrs.mongodb.net'
+// const URI = 'mongodb+srv://Piedriz:Arepa300-@touristapp-db.gcjrs.mongodb.net'
+const URI = 'mongodb://mongo:JZvrLqoflNpuVF6WHdey@containers-us-west-123.railway.app:6058'
 
+//  "mongodb://mongo:JZvrLqoflNpuVF6WHdey@containers-us-west-123.railway.app:6058"
 
 mongoose.connect(URI)
     .then(db => console.log("correct"))
